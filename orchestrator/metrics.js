0609e5033e96032d7d9b563df6e320c82defb5e4
Pablo Romeo <pablo.romeo@gmail.com>
2020-04-18 11:53:52 -0300

    New metrics and some refactoring

--- orchestrator/metrics.js
+++ orchestrator/metrics.js
@@ -2,10 +2,40 @@ const client = require('prom-client');
 
 //client.collectDefaultMetrics()
 
-const gaugeActiveWorkers = new client.Gauge({
-    name: 'active_workers',
-    help: 'Active Workers'
+const counterJobsPosted = new client.Counter({
+    name: 'jobs_posted',
+    help: 'Jobs Posted'
   })
+
+const counterJobsCompleted = new client.Counter({
+    name: 'jobs_completed',
+    help: 'Jobs Completed'
+})
+
+const counterJobsSucceeded = new client.Counter({
+    name: 'jobs_succeeded',
+    help: 'Jobs Succeeded'
+})
+
+const counterJobsFailed = new client.Counter({
+    name: 'jobs_failed',
+    help: 'Jobs Failed'
+})
+
+const counterJobsKilled = new client.Counter({
+    name: 'jobs_killed',
+    help: 'Jobs Killed'
+})
+
+const gaugeJobPosters = new client.Gauge({
+    name: 'job_posters_active',
+    help: 'Active Job Posters'
+})
+
+const gaugeWorkers = new client.Gauge({
+    name: 'workers_active',
+    help: 'Active Workers'
+})
 
 const workerLoadCPUStats = new client.Gauge({
     name : 'worker_load_cpu',
@@ -13,13 +43,24 @@ const workerLoadCPUStats = new client.Gauge({
     labelNames: ['worker_id', 'worker_name'],
 })
 
+const workerLoadTasksStats = new client.Gauge({
+    name : 'worker_load_tasks',
+    help : 'Worker Load - Tasks Count',
+    labelNames: ['worker_id', 'worker_name'],
+})
+
 module.exports = {
-    setActiveWorkers : (amount) => {
-        gaugeActiveWorkers.set(amount)
-    },
-    setWorkerLoadCPU : (workerId, workerName, value) => {
-        workerLoadCPUStats.labels(workerId, workerName).set(value)
-    },
+    jobPosted : () => { counterJobsPosted.inc(1) },
+    jobCompleted : () => { counterJobsCompleted.inc(1) },
+    jobSucceeded : () => { counterJobsSucceeded.inc(1) },
+    jobFailed : () => { counterJobsFailed.inc(1) },
+    jobKilled : () => { counterJobsKilled.inc(1) },
+
+    setActiveWorkers : (amount) => { gaugeWorkers.set(amount) },
+    setActiveJobPosters : (amount) => { gaugeJobPosters.set(amount) },
+    setWorkerLoadCPU : (workerId, workerName, value) => { workerLoadCPUStats.labels(workerId, workerName).set(value) },
+    setWorkerLoadTasks : (workerId, workerName, value) => { workerLoadTasksStats.labels(workerId, workerName).set(value) },
+    
     injectMetricsRoute : (app) => {  
         app.get('/metrics', (req, res) => {
             res.set('Content-Type', client.register.contentType);
