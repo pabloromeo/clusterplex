f9a6e88a5a4296b4d8c3bf17e00051a6693fbf01
Pablo Romeo <pablo@cloudx.com>
2023-09-07 14:19:42 -0300

    Upgrading EAE version

--- worker/app/start.sh
+++ worker/app/start.sh
@@ -5,7 +5,7 @@ cd /usr/lib/plexmediaserver
 CLUSTERPLEX_PLEX_VERSION=$(strings "pms_original" | grep -P '^([0-9]+)\.([0-9]+)\.([0-9]+)\.([0-9]+)-[0-9a-f]{9}')
 CLUSTERPLEX_PLEX_CODECS_VERSION=$(strings "Plex Transcoder" | grep -Po '[0-9a-f]{7}-[0-9]{4}$')
 CLUSTERPLEX_PLEX_EAE_VERSION=$(printf "eae-`strings "pms_original" | grep -P '^EasyAudioEncoder-eae-[0-9a-f]{7}-$' | cut -d- -f3`-42")
-EAE_VERSION=1785 # fixed for now
+EAE_VERSION=1806 # fixed for now
 
 echo "CLUSTERPLEX_PLEX_VERSION => '${CLUSTERPLEX_PLEX_VERSION}'"
 echo "CLUSTERPLEX_PLEX_CODECS_VERSION => '${CLUSTERPLEX_PLEX_CODECS_VERSION}'"
