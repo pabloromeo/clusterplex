11b978f5c98a69dd5640d1cf6f576c2f90100204
Pablo Romeo <pablo@cloudx.com>
2024-04-19 01:51:57 -0300

    updating EAE to v2001

--- worker/app/start.sh
+++ worker/app/start.sh
@@ -6,7 +6,7 @@ CLUSTERPLEX_PLEX_VERSION=$(strings "pms_original" | grep -P '^([0-9]+)\.([0-9]+)
 CLUSTERPLEX_PLEX_CODECS_VERSION=$(strings "Plex Transcoder" | grep -Po '[0-9a-f]{7}-[0-9]{4}$')
 CLUSTERPLEX_PLEX_EAE_VERSION=$(printf "eae-`strings "pms_original" | grep -P '^EasyAudioEncoder-eae-[0-9a-f]{7}-$' | cut -d- -f3`-42")
 
-EAE_VERSION="${EAE_VERSION_NUMBER:=1983}" # default fixed for now
+EAE_VERSION="${EAE_VERSION_NUMBER:=2001}" # default fixed for now
 
 echo "CLUSTERPLEX_PLEX_VERSION => '${CLUSTERPLEX_PLEX_VERSION}'"
 echo "CLUSTERPLEX_PLEX_CODECS_VERSION => '${CLUSTERPLEX_PLEX_CODECS_VERSION}'"
